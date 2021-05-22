4e086800e5f3c1009756f0d1c69c1f97c799a23d
Anjani Sowmya Bollapragada <asbollapragada@gmail.com>
2021-05-22 14:44:57 +0530

    Made Table a real component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
 import React from "react"
 import Table from "./components/Table";
 
-class App extends React.Component{
-    render(){
+class App extends React.Component {
+
+    render() {
+        const dataSource = [
+            {
+                key: 1,
+                qual: "Tenth (CBSE)",
+                yop: 2017,
+                inst: "Sri Prakash Vidyaniketan",
+                gpa: "10 GPA"
+            },
+            {
+                key: 2,
+                qual: "Inter (SSC)",
+                yop: 2019,
+                inst: "Ascent Junior College",
+                gpa: "9.67 GPA"
+            },
+            {
+                key: 3,
+                qual: "Engineering (CSE)",
+                yop: 2023,
+                inst: "Andhra University College of Engieering",
+                gpa: "Still pursuing"
+            }
+        ];
+        const columns = [
+            {
+                key: "qual",
+                title: "Qualification",
+            },
+            {
+                key: "yop",
+                title: "Year of Passing",
+            },
+            {
+                key: "inst",
+                title: "Institution",
+            },
+            {
+                key: "gpa",
+                title: "GPA",
+            },
+        ];
+
         return(
             <div className="App">
-                <Table/>
+                <Table dataSource={dataSource} columns={columns}/>
             </div>
         )
     }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,81 +1,34 @@
 import React from 'react'
 import "./Table.css"
-const TableData = {
-    data: [
-        {
-            key: 1,
-            qualification: "Tenth (CBSE)",
-            yop: 2017,
-            institution: "Sri Prakash Vidyaniketan",
-            gpa: "10 GPA"
-        },
-        {
-            key: 2,
-            qualification: "Inter (SSC)",
-            yop: 2019,
-            institution: "Ascent Junior College",
-            gpa: "9.67 GPA"
-        },
-        {
-            key: 3,
-            qualification: "Engineering (CSE)",
-            yop: 2023,
-            institution: "Andhra University College of Engieering",
-            gpa: "Still pursuing"
-        }
-    ],
-    columns: [
-        {
-            key: "SNo",
-            title: "SNo",
-        },
-        {
-            key: "Qualification",
-            title: "Qualification",
-        },
-        {
-            key: "YOP",
-            title: "Year of Passing",
-        },
-        {
-            key: "Institution",
-            title: "Institution",
-        },
-        {
-            key: "GPA",
-            title: "GPA",
-        },
-
-    ]
-}
 
 const Table = (props) => {
-    const table_data = TableData.data.map((item) =>{
-        return (
-            <tbody>
-                <tr>
-                    <th scope="row">{item.key}</th>
-                    <td>{item.qualification}</td>
-                    <td>{item.yop}</td>
-                    <td>{item.institution}</td>
-                    <td>{item.gpa}</td>
-                </tr>
-            </tbody>
-        )
-    });
-
     const table_columns = 
         <thead>
             <tr>
                 {
-                    TableData.columns.map((item) => {
+                    props.columns.map((column) => {
                         return (
-                            <th scope="col">{item.title}</th>
+                            <th scope="col">{column.title}</th>
                         )
                     })
                 }
             </tr>
         </thead>
+    const table_data = props.dataSource.map((item) =>{
+        return (
+            <tbody>
+                <tr>
+                    {
+                        props.columns.map((column) => {
+                            return (
+                                <td>{item[column.key]}</td>
+                            )
+                        })
+                    }
+                </tr>
+            </tbody>
+        )
+    })
 
     return(
         <div className="container">
